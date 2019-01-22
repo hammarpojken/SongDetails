954a29ea1777df383023777e82f54e50afe54cf8
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-22 09:14:03 +0100

    Add component for showing the selected song

---
+++ src/components/SongDetail.js
@@ -0,0 +1,23 @@
+import { connect } from "react-redux";
+
+import React from "react";
+
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div className="ui segment">
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
+};
+const mapSteteToProps = state => {
+  return { song: state.selectedSong };
+};
+export default connect(mapSteteToProps)(SongDetail);
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -15,7 +21,7 @@ class SongList extends Component {
     });
   }
   render() {
-    return <div className="ui divided list">{this.renderList()}</div>;
+    return <div className="ui divided list segment">{this.renderList()}</div>;
   }
 }
 
@@ -25,4 +31,7 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong: selectSong }
+)(SongList);
